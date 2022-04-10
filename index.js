4af7475ee16179b748f1a17a21dba47a61852cfd
twincy modi <twincymodi@gmail.com>
2022-04-10 18:01:35 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+function returnFirstTwoDrivers(firstTwo){
+    return firstTwo = ["Antonia", "Nuru"]
+}
+
+function returnLastTwoDrivers(lastTwo){
+    return lastTwo = ["Amari", "Mo"]
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(number){
+    return function(number2){
+        return number*number2
+    }
+}
+
+function fareDoubler(value){
+    return value*2
+}
+
+function fareTripler(value){
+    return value*3
+}
+
+function selectDifferentDrivers(arrayOfDrivers, anyTwo){
+    return anyTwo(arrayOfDrivers)
+}
